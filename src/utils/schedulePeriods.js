f97b57e1d9906b7f298e106b89ec6620b1013b42
juninalmeida <junioralmeidati2023@gmail.com>
2026-01-18 15:36:24 -0300

    feat(utils): add schedule period grouping helpers

---
+++ src/utils/schedulePeriods.js
@@ -0,0 +1,34 @@
+import dayjs from "../libs/dayjs.js";
+
+const toMsSafe = (startAtISO) => {
+  const d = dayjs(startAtISO);
+  return d.isValid() ? d.valueOf() : Number.POSITIVE_INFINITY;
+};
+
+export const getPeriodKey = (startAtISO) => {
+  const d = dayjs(startAtISO);
+  if (!d.isValid()) return "morning";
+
+  const hour = d.hour();
+  if (hour < 12) return "morning";
+  if (hour < 18) return "afternoon";
+  return "night";
+};
+
+export const sortByStartAtAsc = (schedules) =>
+  [...schedules].sort((a, b) => toMsSafe(a.startAt) - toMsSafe(b.startAt));
+
+export const groupSchedulesByPeriod = (schedules) => {
+  const grouped = { morning: [], afternoon: [], night: [] };
+
+  for (const s of sortByStartAtAsc(schedules)) {
+    grouped[getPeriodKey(s.startAt)].push(s);
+  }
+
+  return grouped;
+};
+
+export const formatTimeHHmm = (startAtISO) => {
+  const d = dayjs(startAtISO);
+  return d.isValid() ? d.format("HH:mm") : "--:--";
+};
